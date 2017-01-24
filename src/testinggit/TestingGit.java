3da44bd34e203879c36d0a1da2720b518943140d
heuwettera <allison.heuwetter@gmail.com>
2017-01-24 08:51:53 -0500

    Version 1

---
+++ src/testinggit/TestingGit.java
@@ -0,0 +1,17 @@
+
+package testinggit;
+
+/**
+ *
+ * @author allis
+ */
+public class TestingGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
